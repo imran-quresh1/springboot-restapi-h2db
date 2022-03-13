23ebdd845fa5b677869ca545b835c7c111506c4d
imran-quresh1 <88880791+imran-quresh1@users.noreply.github.com>
2022-03-13 14:37:19 +0000

    Add files via upload

---
+++ springboot-restapi-h2db/src/main/java/com/imran/customer/model/Customer.java
@@ -0,0 +1,62 @@
+package com.imran.customer.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstname;
+    private String lastname;
+    // format YYYY-MM-DD
+    private String dateOfBirth;
+
+    public Customer(String firstname, String lastname, String dateOfBirth) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    public Customer() {
+
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public String getDateOfBirth() {
+        return dateOfBirth;
+    }
+
+    public void setDateOfBirth(String dateOfBirth) {
+        this.dateOfBirth = dateOfBirth;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "id=" + id +
+                ", firstname='" + firstname + '\'' +
+                ", lastname='" + lastname + '\'' +
+                ", dateOfBirth='" + dateOfBirth + '\'' +
+                '}';
+    }
+}
---
+++ springboot-restapi-h2db/src/main/java/com/imran/customer/repository/CustomerRepository.java
@@ -0,0 +1,9 @@
+package com.imran.customer.repository;
+
+import com.imran.customer.model.Customer;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerRepository extends CrudRepository<Customer, Long> {
+}
