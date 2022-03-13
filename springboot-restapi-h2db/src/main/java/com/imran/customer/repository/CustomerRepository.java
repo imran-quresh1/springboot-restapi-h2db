e2ac2c0bac5895cd914ce3b50bac07323cd1360e
imran-quresh1 <88880791+imran-quresh1@users.noreply.github.com>
2022-03-13 17:59:05 +0000

    Delete springboot-restapi-h2db directory

--- springboot-restapi-h2db/src/main/java/com/imran/customer/repository/CustomerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.imran.customer.repository;
-
-import com.imran.customer.model.Customer;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface CustomerRepository extends CrudRepository<Customer, Long> {
-}
